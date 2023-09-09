ec80bc648b8d6a62b2f0c1d7dea57aafe3e858c6
SakT1122 <sak.978tirbhane@gmail.com>
2023-09-09 18:24:49 +0530

    pdf_downloader

--- src/main/java/com/example/GrowSkills/Controller/CourseController.java
+++ src/main/java/com/example/GrowSkills/Controller/CourseController.java
@@ -3,18 +3,26 @@ package com.example.GrowSkills.Controller;
 import com.example.GrowSkills.Dtos.CourseEntryDto;
 import com.example.GrowSkills.Models.Course;
 import com.example.GrowSkills.Service.CourseService;
+import com.example.GrowSkills.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.ByteArrayInputStream;
 import java.util.List;
 
 @RestController
 public class CourseController {
     @Autowired
     private CourseService courseService;
+    @Autowired
+    private CustomerService customerService;
     @PostMapping("/add-course")
     private String addCourse(@RequestBody CourseEntryDto courseEntryDto){
         return courseService.addCourse(courseEntryDto);
@@ -23,5 +31,15 @@ public class CourseController {
     @GetMapping("/get-all-courses")
     private List<Course> getAllCourses(){
         return courseService.seeAllCourses();
+    }
+    @PostMapping("/downloadPdf")
+    public ResponseEntity<InputStreamResource> downloadPdf(){
+        ByteArrayInputStream pdf=courseService.downloadPdf();
+        HttpHeaders httpHeaders=new HttpHeaders();
+        httpHeaders.add("Content-Disposition","inline;file=lcwd.pdf");
+        return ResponseEntity.ok()
+                .headers(httpHeaders)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(new InputStreamResource(pdf));
     }
 }
--- src/main/java/com/example/GrowSkills/Service/CourseService.java
+++ src/main/java/com/example/GrowSkills/Service/CourseService.java
@@ -3,14 +3,24 @@ package com.example.GrowSkills.Service;
 import com.example.GrowSkills.Dtos.CourseEntryDto;
 import com.example.GrowSkills.Models.Course;
 import com.example.GrowSkills.Repository.CourseRepository;
+import com.lowagie.text.Document;
+import com.lowagie.text.Font;
+import com.lowagie.text.FontFactory;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.pdf.PdfWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.util.List;
 
 @Service
 public class CourseService {
 
+    private Logger logger= LoggerFactory.getLogger(CustomerService.class);
     @Autowired
     private CourseRepository courseRepository;
 
@@ -30,4 +40,22 @@ public class CourseService {
     public List<Course> seeAllCourses(){
         return courseRepository.findAll();
     }
+
+    public ByteArrayInputStream downloadPdf(){
+        logger.info("Create pdf started");
+        String title="Welcome to Grow Skill";
+        String content="Congratulations for successful course completion";
+        ByteArrayOutputStream out=new ByteArrayOutputStream();
+        Document document=new Document();
+        PdfWriter.getInstance(document,out);
+        document.open();
+        Font titleFont= FontFactory.getFont(FontFactory.HELVETICA_BOLD,20);
+        Paragraph paragraph=new Paragraph(title,titleFont);
+        document.add(paragraph);
+        Paragraph paragraph1=new Paragraph(content);
+        document.add(paragraph1);
+        document.close();
+        return new ByteArrayInputStream(out.toByteArray());
+    }
+
 }
